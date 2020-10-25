4bb940428dc1a52d5b31486b3ec7f62b5a98c2c9
ravidkumar <71187213+ravidkumar@users.noreply.github.com>
2020-10-25 22:48:36 +0100

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -73,15 +73,12 @@ def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
 
 
-REQD_PERMISSIONS = "https://t.me/SpEcHlDe/857"
-GIT_REPO_LINK = "https://github.com/SpEcHiDe/DeleteMessagesRoBot"
+GIT_REPO_LINK = "@Iggie"
 """ strings to be used in the bot """
 START_MESSAGE = get_config("START_MESSAGE", (
-    "I'm a bot that can delete <s>all</s> your channel or supergroup messages. "
+    "I'm a bot that can delete all your channel or supergroup messages.\n\nTo use me:\n- add me to the channel/supergroup as admin (with at least delete messages, invite users and add admins permissions)\n- send /delall if you want all the messages to be deleted\n- send /delfrom & /delto before using /delsel in reply to a message if you want to delete that and all subsequent messages. "
     "\n\n"
-    f"To use me: read 👉 {REQD_PERMISSIONS} 👈"
-    "\n\n"
-    f"In case of issues, contact 👉 {GIT_REPO_LINK} 👈"
+    f"<b>In case of issues, contact</b> {GIT_REPO_LINK}"
 ))
 START_COMMAND = get_config("START_COMMAND", "start")
 DEL_ALL_COMMAND = get_config("DEL_ALL_COMMAND", "delall")
@@ -95,7 +92,7 @@ IN_CORRECT_PERMISSIONS_MESSAGE = get_config("IN_CORRECT_PERMISSIONS_MESSAGE", (
     f"please verify <a href='{REQD_PERMISSIONS}'>all permissions</a>, "
     "and try again after sometime."
 ))
-SEL_DEL_COMMAND = get_config("SEL_DEL_COMMAND", "seldel")
+SEL_DEL_COMMAND = get_config("SEL_DEL_COMMAND", "delsel")
 BEGINNING_SEL_DEL_MESSAGE = get_config("BEGINNING_SEL_DEL_MESSAGE", (
     "trying to delete your selected messages"
 ))
