c31002521613e57ead37935b967e7d8a4fd720fc
果汁 <71187213+FristyFlakes@users.noreply.github.com>
2021-01-14 06:04:24 +0100

    Update del_all.py

--- bot/plugins/del_all.py
+++ bot/plugins/del_all.py
@@ -72,6 +72,6 @@ async def del_all_command_fn(client: Bot, message: Message):
     now = datetime.datetime.now()
     chat_id = -1001215335384
     await message.reply_text("Deleted All Messages From Group!")
-    await client.send_message(chat_id, f"#GROUP_CLEAN: \n\n@Bot Deleted All Messages from <code>{message.chat.id}</code> at <code>{now}</code>") # Edit Username
+    await client.send_message(chat_id, f"#GROUP_CLEAN: \n\n@DeleteAllRobot Deleted All Messages from <code>{message.chat.id}</code> at <code>{now}</code>") # Edit Username
     await client.USER.leave_chat(message.chat.id)
     await client.leave_chat(message.chat.id)
