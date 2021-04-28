d2ec9696835282195bb361cef9d0f64be276c0fe
女神 <53878781+RomanGoddess@users.noreply.github.com>
2021-04-28 16:55:01 +0100

    Update make_user_join_chat.py

--- bot/helpers/make_user_join_chat.py
+++ bot/helpers/make_user_join_chat.py
@@ -1,41 +1,46 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) Shrimadhav U K | @AbirHasan2005
-#
-# This is first coded by Shrimadhav Sir.
-# Thanks a lot for the nice script.
-#
-# This is modified by @AbirHasan2005 for
-# DMCA Delete Bot: @DMCA_DelBot 
+# (c) Shrimadhav U K
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
 from pyrogram.errors import (
     InviteHashExpired,
     InviteHashInvalid,
-    UserAlreadyParticipant,
-    ChatAdminRequired
+    UserAlreadyParticipant
 )
-from pyrogram.types import Chat
+from pyrogram.types import Message
 from bot.bot import Bot
 
 
 async def make_chat_user_join(
     client: Bot,
     user_id: int,
-    chat: Chat
+    message: Message
 ):
-    chat_invite_link = await chat.export_invite_link()
+    chat_invite_link = await message.chat.export_invite_link()
     try:
-        await client.join_chat(chat_invite_link) #
-    except ChatAdminRequired:
-        return False, "Sorry Sir, I am not Admin in `{}` !!\n\nPlease add me as Admin in that chat will **Delete Messages** & **Add Admins** rights."
+        await client.join_chat(chat_invite_link)
     except UserAlreadyParticipant:
         pass
-    _existing_permissions = await chat.get_member(user_id)
+    except (InviteHashExpired, InviteHashInvalid) as e:
+        return False, str(e)
+    _existing_permissions = await message.chat.get_member(user_id)
     if _existing_permissions.status == "creator":
         return True, None
     if not _existing_permissions.can_delete_messages:
-        await chat.promote_member(
+        await message.chat.promote_member(
             user_id,
             can_change_info=False,
             can_post_messages=False,
