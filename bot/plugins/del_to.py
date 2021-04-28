ad3cf787bee854be7f4f997d9f7ec8ade5880806
女神 <53878781+RomanGoddess@users.noreply.github.com>
2021-04-28 15:13:31 +0100

    Update del_to.py

--- bot/plugins/del_to.py
+++ bot/plugins/del_to.py
@@ -48,8 +48,7 @@ async def del_to_command_fn(client: Bot, message: Message):
     ] = message.reply_to_message.message_id
     if status_message:
         await status_message.edit_text(
-            "End Point Saved message_id. "
-            "https://t.me/HowToUseDeleteAllRobot/2"
+            "Saved Delete Ending Message ID to Database!"
+            ""
+            "Now send /delsel to Start Deleting Selected Messages!"
         )
-        await status_message.delete()
-    await message.delete()
