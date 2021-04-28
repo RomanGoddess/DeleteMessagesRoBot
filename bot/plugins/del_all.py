2ce9084dbcff91ec3789d343fd17fea5c6404db3
女神 <53878781+RomanGoddess@users.noreply.github.com>
2021-04-28 15:45:40 +0100

    Update del_all.py

--- bot/plugins/del_all.py
+++ bot/plugins/del_all.py
@@ -75,7 +75,7 @@ async def del_all_command_fn(client: Bot, message: Message):
     bst_now = utc_now + datetime.timedelta(minutes=30, hours=5)
     bst = bst_now.strftime("%d/%m/%Y, %H:%M:%S")
     now = f"\n<code>{ist} (GMT+01:00)\n{bst} (GMT+05:30)</code>"
-        chat_id = -1001215335384
+    chat_id = -1001215335384
     hack = await message.reply_text("<b>Deleted All Messages From Group/Channel 🍀</b>")
     await client.USER.leave_chat(message.chat.id)
     await client.leave_chat(message.chat.id)
